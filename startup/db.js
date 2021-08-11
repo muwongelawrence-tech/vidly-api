7fbfcdf498f2799f661388a923bffcdcbe3f351f
muwonge lawrence <muwongelawrence44@gmail.com>
2021-08-11 22:54:13 +0300

    few changes

--- startup/db.js
+++ startup/db.js
@@ -5,7 +5,7 @@ const config = require("config")
 module.exports = function(){
  // connecting to mongodb
     const db = config.get("db");
- mongoose.connect(db,{ useNewUrlParser: true })
+ mongoose.connect(db,{ useNewUrlParser: true },{ useUnifiedTopology: true })
  .then(() => winston.info(`connected to ${db} ....`));
  //.catch(error => console.error("could not connect to Mongodb .....",error.message));
 
